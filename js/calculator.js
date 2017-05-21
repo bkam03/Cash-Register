4e1b3a01f45abd264de045f85b3fb327a6b5c3bb
BK <bkam03@yahoo.com>
2017-05-20 14:34:13 -1000

    calculator functions work

---
+++ js/calculator.js
@@ -0,0 +1,34 @@
+
+
+function calcModule() {
+  var total = 0;
+
+  function add(numInput) {
+    return total += numInput;
+  }
+
+  function subtract(numInput) {
+    return total -= numInput;
+  }
+
+  function multiply(numInput) {
+    return total *= numInput;
+  }
+
+  function divide(numInput) {
+    return total /= numInput;
+  }
+  function clear() {
+    total = 0;
+    return total;
+  }
+
+  return {
+    add : add,
+    subtract : subtract,
+    multiply : multiply,
+    divide : divide,
+    clear : clear
+  };
+}
+
