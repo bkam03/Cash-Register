6d153ac0568db9c3b1cd8086b0b90873470a9ccc
Ross Wu <RAWSE-DA-BAWSE@Rosss-MacBook-Pro.local>
2017-05-20 15:54:46 -1000

    shiiiiiiii*******

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,7 +1,17 @@
 function cashModule() {
-  var amount = 0;
+  var amount = [0];
   var balance = 0;
   var calculator = calcModule();
+
+  function inputNumber(){
+    amount.push(this.innerHTML);
+    display(amount);
+  }
+
+  function display (inputDisplay){
+    var node = document.querySelector("#display");
+    node.innerHTML = inputDisplay;
+  }
 
   function getBalance() {
     return balance;
@@ -18,7 +28,17 @@ function cashModule() {
   return {
     getBalance : getBalance,
     depositCash : depositCash,
-    withdrawalCash : withdrawalCash
+    withdrawalCash : withdrawalCash,
+    display : display,
+    inputNumber :inputNumber
   };
+}
+
+var cashRegister = new cashModule();
+var buttons = document.querySelectorAll(".number");
+
+for (var i = 0; i < buttons.length; i++ ){
+  buttons[i].addEventListener('click', cashRegister.inputNumber());
+  console.log(buttons[i].innerHTML);
 }
 
