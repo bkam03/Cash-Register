2f8efe250d81d629f2db1061b9c3b693009e3b68
BK <bkam03@yahoo.com>
2017-05-20 15:25:07 -1000

    money functions added

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,1 +1,24 @@
-console.log("hello");
+function cashModule() {
+  var amount = 0;
+  var balance = 0;
+  var calculator = calcModule();
+
+  function getBalance() {
+    return balance;
+  }
+
+  function depositCash(cash) {
+    balance += cash;
+  }
+
+  function withdrawalCash(cash) {
+    balance -= cash;
+  }
+
+  return {
+    getBalance : getBalance,
+    depositCash : depositCash,
+    withdrawalCash : withdrawalCash
+  };
+}
+
