39eee5abd58ba6f061d0258f2ae2046bd61e650c
BK <bkam03@yahoo.com>
2017-05-23 15:22:06 -1000

    added validations, cleaned up code

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,83 +1,114 @@
 function cashModule() {
   var amount = [];
-  var balance = 220;
+  var balance = 0;
   var calculator = calcModule();
 
-  function removeBrackets(remove) {
+  function removeBrackets( remove ) {
     // return remove.charAt(2);
-    return remove.slice(2, remove.length-2);
+    return remove.slice( 2, remove.length-2 );
   } // removes brackets from array to display just number
 
 
   function input(){
-    amount.push(removeBrackets(this.innerHTML));
-    display(amount);
+    var keyPress = removeBrackets( this.innerHTML );
+
+    //validation for operator repeats/chains
+    var operatorArray = [ "+", "-", "/", "*" ];
+//check if there are 2 operators in a row, allow replacement
+  //if keyPress is an operator,
+  if( operatorArray.indexOf( keyPress ) > -1 ) {
+    //check if last input was operator. if so delete previous operator.
+    if( operatorArray.indexOf( amount[ amount.length - 1 ] ) > -1 ) {
+      amount.pop();
+      display( amount );
+    }
+  }
+    amount.push( keyPress );
+    display( amount );
   } // display number (without brackets) that users click
 
-  function display (inputDisplay){
-    var node = document.querySelector("#display");
-    if (Array.isArray(inputDisplay)){
-    node.innerHTML = inputDisplay.join('');
-      }else{
+  function display( inputDisplay ){
+    var node = document.querySelector( "#display" );
+    if ( Array.isArray( inputDisplay ) ){
+    node.innerHTML = inputDisplay.join( '' );
+      } else {
         node.innerHTML = inputDisplay;
       }
   } // display input users click
 
+  function resetAmount() {
+    amount = [];
+  }  //reset the amount
+
+  //public functions
+
   function getBalance() {
-    amount = [balance];
-    display(amount);
+    amount = [ balance ];
+    display( amount );
   } // display balance
 
   function clear() {
     calculator.clear();
-    amount = [0];
-    display(calculator.getTotal());
+    resetAmount();
+    display( calculator.getTotal() );
   }
 
   function depositCash() {
-
-    balance += extractNum(amount);
-    //console.log(balance);
+    var depositing = calculation();
     clear();
+    if( depositing > 0 ) {
+      balance += depositing;
+    } else {
+      display( "invalid action" );
+    }
   }
 
   function withdrawalCash() {
-    balance -= extractNum(amount);
+    var withdrawing = calculation();
+    if( withdrawing <= balance ) {
+      balance -= withdrawing;
+      display( balance );
+    } else {
+      resetAmount();
+      display( "insufficient funds" );
+    }
     //console.log(balance);
-    clear();
   }
 
   function extractNum(){
     var number = "";
-    while(!isNaN(amount[0]) || amount[0] === "."){
+    while( !isNaN( amount[ 0 ] ) || amount[ 0 ] === "." ){
       number = number + amount.shift();
     }
-    return Number(number);
+    return Number( number );
 
   }
 
   function calculation(){
-    if (!isNaN(amount[0])){
+    var total = 0;
+
+    if ( !isNaN(amount[ 0 ] ) ) {
       calculator.clear();
-      calculator.add(extractNum());
+      calculator.add( extractNum() );
     }
 
     //loop while amount array.length > 0
-      while(amount.length > 0){
+      while( amount.length > 0 ) {
         var operator = amount.shift();
         var currentNum = extractNum();
-        if (operator === "+"){
-          calculator.add(currentNum);
-        }else if (operator === "-"){
-          calculator.subtract(currentNum);
-        }else if (operator === "*"){
-          calculator.multiply(currentNum);
-        }else if (operator === "/"){
-          calculator.divide(currentNum);
+        if ( operator === "+" ) {
+          calculator.add( currentNum );
+        } else if ( operator === "-" ) {
+          calculator.subtract( currentNum );
+        } else if ( operator === "*" ){
+          calculator.multiply( currentNum );
+        } else if ( operator === "/" ){
+          calculator.divide( currentNum );
         }
       }
-
-      display(calculator.getTotal());
+      total = calculator.getTotal();
+      display( total );
+      return total;
   }
 
   return {
@@ -93,18 +124,18 @@ function cashModule() {
 
 var cashRegister = cashModule(); // turn cashModule into an object
 
-var buttons = document.querySelectorAll(".number"); // buttons call for class "number" in HTML
-for (var i = 0; i < buttons.length; i++ ){
-  buttons[i].addEventListener('click', cashRegister.input);
+var buttons = document.querySelectorAll( ".number" ); // buttons call for class "number" in HTML
+for ( var i = 0; i < buttons.length; i++ ) {
+  buttons[ i ].addEventListener( 'click', cashRegister.input );
 } // loops though class "number" (from HTML) and makes it run input function when clicked
 
-buttons = document.querySelectorAll(".operations"); // reassign buttons to call class "operation"
-for( var i = 0; i < buttons.length;i++) {
-  buttons[i].addEventListener('click', cashRegister.input);
+buttons = document.querySelectorAll( ".operations" ); // reassign buttons to call class "operation"
+for( var i = 0; i < buttons.length; i++ ) {
+  buttons[ i ].addEventListener( 'click', cashRegister.input );
 }// loops though class "operations" (from HTML) and makes it run input function when clicked
 
-balance.addEventListener("click", cashRegister.getBalance); //run getBalance function when clicked
-equal.addEventListener("click",cashRegister.calculation);
-clear.addEventListener("click", cashRegister.clear);
-deposit.addEventListener("click", cashRegister.depositCash);
-withdraw.addEventListener("click", cashRegister.withdrawalCash);
+balance.addEventListener( "click", cashRegister.getBalance ); //run getBalance function when clicked
+equal.addEventListener( "click",cashRegister.calculation );
+clear.addEventListener( "click", cashRegister.clear );
+deposit.addEventListener( "click", cashRegister.depositCash );
+withdraw.addEventListener( "click", cashRegister.withdrawalCash );
